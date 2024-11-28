ef768d49dc466444a8aa99d40b09d97991718856
Jean-Baptiste <j.baptiste.ledig@icloud.com>
2024-11-28 20:55:45 +0100

    Configure tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App/App'
+import './index.css'
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root')!)
